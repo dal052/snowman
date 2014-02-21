14181ebbfa007831c574c309d9019fb5b129dfc4
james <msiyaulee@gmail.com>
2014-02-21 03:10:07 -0800

    try to fix Card.java

--- snowman/src/com/androidstudio/snowman/aux/Card.java
+++ snowman/src/com/androidstudio/snowman/aux/Card.java
@@ -8,56 +8,81 @@ public class Card implements Parcelable {
 	private String front;
 	private String back;
 	private int id;
-	
+
 	public Card(String group, String front, String back) {
 		this.group = group;
 		this.front = front;
 		this.back = back;
 	}
-	
-	 /**
-     * Default Constructor
-     */
-    public Card() {}
 
-    /**
-     * Getter and setters for all instance variables
-     */
-    public int getId() {
-        return id;
-    }
+	/**
+	 * Default Constructor
+	 */
+	public Card() {}
 
-    public void setId(int id) {
-        this.id = id;
-    }
-    
-    public String getFront() {
-    	return front;
-    }
-    
-    public void setFront(String front){
-    	this.front = front;
-    }
-    
-    public String getBack() {
-    	return back;
-    }
-    
-    public void setBack(String back) {
-    	this.back = back;
-    }
+	/**
+	 * Getter and setters for all instance variables
+	 */
+	public int getId() {
+		return id;
+	}
 
-//I forgot to save the rest....TT
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getGroup() {
+		return group;
+	}
+	public void setGroup(String group) {
+		this.group = group;
+	}
+
+	public String getFront() {
+		return front;
+	}
+
+	public void setFront(String front){
+		this.front = front;
+	}
+
+	public String getBack() {
+		return back;
+	}
+
+	public void setBack(String back) {
+		this.back = back;
+	}
+
 
 	@Override
 	public int describeContents() {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public void writeToParcel(Parcel dest, int flags) {
-		// TODO Auto-generated method stub
-		
+		dest.writeString(group);
+		dest.writeString(front);
+		dest.writeString(back);
 	}
+
+	public Card(Parcel in) {
+		this.group = in.readString();
+		this.front = in.readString();
+		this.back = in.readString();
+	}
+
+	public static final Parcelable.Creator<Card> CREATOR 
+	= new Parcelable.Creator<Card>() {
+
+		public Card createFromParcel(Parcel in) {
+			return new Card(in);
+		}
+
+		public Card[] newArray(int size) {
+			return new Card[size];
+		}
+	};
+
 }
