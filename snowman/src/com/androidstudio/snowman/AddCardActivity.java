03391a5368d1b46450226c5fb5bb94f290efc7f7
John E Kim <John E Kim@JohnEKim-PC>
2014-04-04 00:32:00 -0700

    added PRASE for cloud data saving

--- snowman/src/com/androidstudio/snowman/AddCardActivity.java
+++ snowman/src/com/androidstudio/snowman/AddCardActivity.java
@@ -1,6 +1,10 @@
 package com.androidstudio.snowman;
 
 import com.androidstudio.snowman.auxiliary.Card;
+import com.parse.Parse;
+import com.parse.ParseAnalytics;
+import com.parse.ParseObject;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
@@ -8,21 +12,27 @@ import android.view.View;
 import android.widget.EditText;
 
 public class AddCardActivity extends Activity {
-	
+
 	private String currentGroup;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_add_card);
-		
+		Parse.initialize(this, "W8YcGfW6gRdv00U1dHHlHdPFK8wKuLIXd0vxSmNl", "65moJJoXhCObFwL9lQGlFV9LSlAo5pKH9rtlE1hr");
+
 		currentGroup = getIntent().getStringExtra(MainActivity.CURRENTGROUP);
+
+//		ParseObject testObject = new ParseObject("TestObject");
+//		testObject.put("foo", "bar");
+//		testObject.saveInBackground();
+		
 	}
-	
+
 	public void addCard(View view){
 		EditText cardBack = (EditText) findViewById(R.id.enter_back);
 		EditText cardFront = (EditText) findViewById(R.id.enter_front);
-		
+
 		//add card to database and show on screen based on clicked button
 		switch(view.getId()){
 		case R.id.click_post:
@@ -30,6 +40,16 @@ public class AddCardActivity extends Activity {
 					cardBack.getText().toString());
 			MainActivity.cardhandler.createCard(newCard);
 			MainActivity.changeInDatabase = true;
+			
+			
+			// Save data to Parse.com :]
+			ParseObject studybuddy = new ParseObject("studybuddies");
+			studybuddy.put("DECK", currentGroup);
+			studybuddy.put("FRONT_PAGE", cardFront.getText().toString());
+			studybuddy.put("BACK_PAGE", cardBack.getText().toString());
+			studybuddy.saveInBackground();
+
+			
 			break;
 		case R.id.click_cancel:
 			break;
@@ -45,7 +65,7 @@ public class AddCardActivity extends Activity {
 		return true;
 	}
 
-	
-	
+
+
 
 }
